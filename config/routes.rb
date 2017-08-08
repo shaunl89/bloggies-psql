259d838573ddc6d53cff4493b6d29a093ce1f527
Shaun Loh <loh.shaun89@gmail.com>
2017-08-08 13:25:37 +0800

    devise setup before adding extra property

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users,
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
+  root 'static#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
